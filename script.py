cc2dfbcb7c4ff7b7829f5342a857b95e2c47a59a
yvpathirana <yasiruvinodpathirana@gmail.com>
2020-07-18 11:32:46 +0530

    first commit

---
+++ script.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template("index.html")
+@app.route('/about')
+def page():
+	return render_template("page.html")
+
+if __name__=="__main__":
+	app.run(debug=True)
