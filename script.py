ff0a096e13bf4bcee3445ca6f4b9599706afaf47
kpyasiru <yasiruvinodpathirana@gmail.com>
2022-09-01 23:38:04 +0530

    Gorilla pages

--- script.py
+++ script.py
@@ -4,13 +4,22 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template("index.html")
+	return render_template("home.html")
 @app.route('/about')
 def page():
 	return render_template("page.html")
 @app.route('/nav')
 def nav():
 	return render_template("navigation.html")
+@app.route('/page1')
+def page1():
+	return render_template("page1.html")
+@app.route('/photo')
+def photo():
+	return render_template("photos.html")
+@app.route('/phy')
+def phy():
+	return render_template("phy.html")
 
 if __name__=="__main__":
 	app.run(debug=True)
